a02f8722097e662d0fdb4e73f6f632ba68e71d68
Wince90 <bswiincent@gmail.com>
2024-10-25 12:14:52 +0200

    add sort and print report

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    #print(text)
-    count_each_character(text)
+    number_of_words = words_counter(text)
+    text_dict = count_each_character(text)
+    sorted_text = sort_dictionary(text_dict)
+    print_report(sorted_text, book_path, number_of_words)
+
 
 
 def get_book_text(path):
@@ -25,7 +28,19 @@ def count_each_character(text):
             else:
                 word_dictionary[word] = 1
             
-    print(word_dictionary)
+    return word_dictionary
 
+def sort_dictionary(dictionary):
+    sorted_scores = dict(sorted(dictionary.items(), key=lambda a: a[1], reverse=True))
+    return sorted_scores
+
+def print_report(process_text, book_path, words_number):
+    tuple_text = process_text.items()
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{words_number} words found in the document\n")
+    for text in tuple_text:
+        if text[0].isalpha():
+            print(f"The '{text[0]}' character was found {text[1]} times")
+    print("--- End report ---")
 
 main()
