5b044f747cc5e25d616fefb0a7768312366fe849
Wince90 <bswiincent@gmail.com>
2024-10-25 10:47:49 +0200

    count each character

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     #print(text)
-    print(f'{words_counter(text)}')
+    count_each_character(text)
 
 
 def get_book_text(path):
@@ -13,6 +13,19 @@ def words_counter(text):
     split_text = text.split()
     numebr_of_words = len(split_text)
     return numebr_of_words
+
+def count_each_character(text):
+    lowered_text = text.lower()
+    split_lower_text = lowered_text.split()
+    word_dictionary = {}
+    for words in split_lower_text:
+        for word in words:
+            if word in word_dictionary:
+                word_dictionary[word] +=1
+            else:
+                word_dictionary[word] = 1
+            
+    print(word_dictionary)
 
 
 main()
